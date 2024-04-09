7a0233daefb0cec8fa6e0f4403d393c6eb2e4ef0
Suraj-Razor <14635@coderacademy.edu.au>
2024-04-09 19:56:29 +1000

    feat: Generate random pokemon

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("Terminal App is running")
+const pokemon = require("pokemon")
+
+let randomPokemon = pokemon.random();
+
+console.log(`Today's lucky Pokemon is ${randomPokemon}`)
+
 console.log("By Bye, terminal app finished!")
